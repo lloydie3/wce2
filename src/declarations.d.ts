06d4e76ef9f56da357401cfb6a739f21de6eb019
Lloyd.Attua <Lloyd.Attua@Shell.com>
2022-09-10 16:51:09 +0100

    new commit

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,8 +1,8 @@
 declare module '*.webp' {
-    const src: string;
-    export default src;
+  const src: string
+  export default src
 }
 declare module '*.jpg' {
-    const src: string;
-    export default src;
+  const src: string
+  export default src
 }
